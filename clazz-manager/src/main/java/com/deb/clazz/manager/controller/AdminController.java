28bca44a6a4c56480f17048f6efad1d5a44ad9e5
沈学海 <8425334@qq.com>
2021-03-22 12:55:36 +0800

    添加mybatis-plus

--- clazz-manager/src/main/java/com/deb/clazz/manager/controller/AdminController.java
+++ clazz-manager/src/main/java/com/deb/clazz/manager/controller/AdminController.java
@@ -1,23 +1,4 @@
 package com.deb.clazz.manager.controller;
 
-import com.deb.clazz.common.entity.Admin;
-import com.deb.clazz.manager.service.AdminService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/admin")
 public class AdminController {
-
-    private final AdminService adminService;
-
-    @PostMapping()
-    public ResponseEntity<Object> create(@RequestBody Admin admin) {
-        adminService.create(admin);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
 }
